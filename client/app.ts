7956a0c5a41f31e96562a117b7cc4a3737ae1e81
barbatus <alex.borodach@gmail.com>
2015-09-26 17:36:13 +0300

    Step 2.3: define parties property

--- client/app.ts
+++ client/app.ts
@@ -9,6 +9,8 @@ import {bootstrap} from 'angular2/bootstrap';
     templateUrl: 'client/app.html'
 })
 class Socially {
+    parties: Array<Object>;
+
     constructor () {
         this.parties = [
           {'name': 'Dubstep-Free Zone',
