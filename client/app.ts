12222b8cf82f057d4adfcd61cd13d039864c9664
barbatus <alex.borodach@gmail.com>
2015-09-26 17:12:43 +0300

    Step 0.5: create root component

---
+++ client/app.ts
@@ -0,0 +1,13 @@
+import {Component, View} from 'angular2/core';
+
+import {bootstrap} from 'angular2/bootstrap';
+
+@Component({
+    selector: 'app'
+})
+@View({
+    template: 'Hello World!'
+})
+class Socially { }
+
+bootstrap(Socially);
