ae63b8c1f3c02d3688b775813656a4748d086f98
barbatus <alex.borodach@gmail.com>
2015-12-20 20:52:38 +0300

    Step 12.15: add search by the party location using Mongo regex

--- server/parties.ts
+++ server/parties.ts
@@ -1,6 +1,6 @@
 import {Parties} from 'collections/parties';
 
-function buildQuery(partyId?: string): Object {
+function buildQuery(partyId: string, location: string): Object {
     var isAvailable = {
         $or: [
             { public: true },
@@ -17,12 +17,15 @@ function buildQuery(partyId?: string): Object {
         return { $and: [{ _id: partyId }, isAvailable] };
     }
 
-    return isAvailable;
+    let searchRegEx = { '$regex': '.*' + (location || '') + '.*', '$options': 'i' };
+
+    return { $and: [{ location: searchRegEx }, isAvailable] };
 }
 
-Meteor.publish('parties', function(options: Object) {
-    Counts.publish(this, 'numberOfParties', Parties.find(buildQuery.call(this)), { noReady: true });
-    return Parties.find(buildQuery.call(this), options);
+Meteor.publish('parties', function(options: Object, location: string) {
+    Counts.publish(this, 'numberOfParties',
+        Parties.find(buildQuery.call(this, null, location)), { noReady: true });
+    return Parties.find(buildQuery.call(this, null, location), options);
 });
 
 Meteor.publish('party', function(partyId: string) {
