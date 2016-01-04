96a7198c548b2419f571f623d92a463ec94007de
barbatus <alex.borodach@gmail.com>
2016-01-04 13:21:15 +0700

    Step 4.9: add addParty method into the component

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 
-import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
+import {FormBuilder, Control, ControlGroup, Validators} from 'angular2/common';
+
+import {Parties} from 'collections/parties';
 
 @Component({
     selector: 'parties-form',
@@ -16,5 +18,19 @@ export class PartiesForm {
             description: [''],
             location: ['', Validators.required]
         });
+    }
+
+    addParty(party) {
+        if (this.partiesForm.valid) {
+            Parties.insert({
+                name: party.name,
+                description: party.description,
+                location: party.location
+            });
+
+            (<Control>this.partiesForm.controls['name']).updateValue('');
+            (<Control>this.partiesForm.controls['description']).updateValue('');
+            (<Control>this.partiesForm.controls['location']).updateValue('');
+        }
     }
 }
